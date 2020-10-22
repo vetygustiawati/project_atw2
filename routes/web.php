20176fb1bff04c40cba53cb93efcbdc3b37f2a72
vetygustiawati <71057826+vetygustiawati@users.noreply.github.com>
2020-10-22 13:30:24 +0700

    update pada halaman web

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/index', function () {
+    return view("index");
+});
+Route::get('/profil', function () {
+    return view("profil");
+});
+Route::get('/product', function () {
+    return view("product");
+});
+Route::get('/transaksi', function () {
+    return view("transaksi");
+});
+Route::get('/login', function () {
+    return view("login");
+});
